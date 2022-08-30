e2ec0600cddb5be58ff57cc636d832a1964e65d9
unknown <geri.ibra@gmail.com>
2022-08-30 15:08:19 +0200

    added clipboard

--- js/topics.js
+++ js/topics.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     let prevBtns =  Array.from(document.getElementsByClassName('button-prev'));
     let footers =  Array.from(document.getElementsByClassName('footer'));
     let timer = null;
+    let clbImgs =  Array.from(document.getElementsByClassName('clipboard-img'));
     document.addEventListener('scroll', (event) => {
         prevBtns.forEach(prevBtn => {
             prevBtn.classList.add('button-fade');
@@ -21,4 +22,16 @@ document.addEventListener('DOMContentLoaded', function(){
             });
         }, 250);
     });
+    clbImgs.forEach(clbImg => {
+        clbImg.addEventListener('click', (event) => {
+            let clickedClb = event.target;
+            clickedClb.classList.add('clicked-clip');
+            let copiedText = clickedClb.parentElement.nextElementSibling.firstElementChild.textContent;
+            navigator.clipboard.writeText(copiedText);
+            setTimeout(function () {
+                clickedClb.classList.remove('clicked-clip');
+            }, 150);
+        });
+    });
+
 });
